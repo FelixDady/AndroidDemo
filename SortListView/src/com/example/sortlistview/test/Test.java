5d4465e6f084d1b31cf975e2b97e2141d04d7699
felix <736232402@qq.com>
2015-09-29 13:25:03 +0800

    test add getter and setter

--- SortListView/src/com/example/sortlistview/test/Test.java
+++ SortListView/src/com/example/sortlistview/test/Test.java
@@ -2,4 +2,12 @@ package com.example.sortlistview.test;
 
 public class Test {
 	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
 }
