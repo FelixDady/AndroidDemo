28117a033d34f380a9d38da4798b52f6ed067566
felix <736232402@qq.com>
2015-09-29 14:06:51 +0800

    this is first version

---
+++ ViewPagerIndicator/src/com/viewpagerindicator/sample/SampleCirclesStyledTheme.java
@@ -0,0 +1,23 @@
+package com.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import com.viewpagerindicator.CirclePageIndicator;
+
+public class SampleCirclesStyledTheme extends BaseSampleActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// The look of this sample is set via a style in the manifest
+		setContentView(R.layout.simple_circles);
+
+		mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPager.setAdapter(mAdapter);
+
+		CirclePageIndicator indicator = (CirclePageIndicator) findViewById(R.id.indicator);
+		indicator.setViewPager(mPager);
+	}
+}
