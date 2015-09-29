ed6f82e24243799f5638bd92e0366aaaf8a09e2c
felix <736232402@qq.com>
2015-09-29 13:17:30 +0800

    this is the demo

---
+++ ViewPagerIndicator/src/com/viewpagerindicator/sample/SampleTitlesInitialPage.java
@@ -0,0 +1,25 @@
+package com.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import com.viewpagerindicator.TitlePageIndicator;
+
+public class SampleTitlesInitialPage extends BaseSampleActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.simple_titles);
+		
+		mAdapter = new TestTitleFragmentAdapter(getSupportFragmentManager());
+		
+		mPager = (ViewPager)findViewById(R.id.pager);
+		mPager.setAdapter(mAdapter);
+		
+		TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
+		indicator.setViewPager(mPager);
+		indicator.setCurrentItem(mAdapter.getCount() - 1);
+		
+		//You can also do: indicator.setViewPager(pager, initialPage);
+	}
+}
---
+++ ViewPagerIndicator/src/com/viewpagerindicator/sample/SampleTitlesStyledLayout.java
@@ -0,0 +1,22 @@
+package com.viewpagerindicator.sample;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import com.viewpagerindicator.TitlePageIndicator;
+
+public class SampleTitlesStyledLayout extends BaseSampleActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.themed_titles);
+		
+		mAdapter = new TestTitleFragmentAdapter(getSupportFragmentManager());
+		
+		mPager = (ViewPager)findViewById(R.id.pager);
+		mPager.setAdapter(mAdapter);
+		
+		TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
+		indicator.setViewPager(mPager);
+	}
+}
